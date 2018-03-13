5548649e6772901006ac6d998042e1cf56c64a42
p2bilt <peteralan@gmail.com>
2018-03-13 15:18:50 -0400

    first files

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// Set up MySQL connection.
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "3mca0djwbgdwnd",
+  database: "cat_db"
+});
+
+// Make connection.
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
