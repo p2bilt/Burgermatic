e6c311647a4620ca59e3b22e82579e286db25f7b
p2bilt <peteralan@gmail.com>
2018-03-20 21:33:51 -0400

    make it work with JawsDB + Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,35 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+const connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "3mca0djwbgdwnd",
+        database: "burgers_db"
+    });
+
+};
+
 
 const connection = mysql.createConnection({
-  host: "us-cdbr-iron-east-05.cleardb.net",
-  user: "b8c6fa9b781d46",
-  password: "dfb8a686",
-  database: "heroku_6cda3c09e407d86"
+    host: "us-cdbr-iron-east-05.cleardb.net",
+    user: "b8c6fa9b781d46",
+    password: "dfb8a686",
+    database: "heroku_6cda3c09e407d86"
 });
 
 // Make connection.
 connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
-  console.log(`connected as id ${connection.threadId}`);
+    if (err) {
+        console.error(`error connecting: ${err.stack}`);
+        return;
+    }
+    console.log(`connected as id ${connection.threadId}`);
 });
 
 // Export connection for our ORM to use.
--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,9 @@ USE burgers_db;
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+	burger_name varchar(255) NOT NULL,burgers_db
 	eaten BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+
+select * from burgers;
